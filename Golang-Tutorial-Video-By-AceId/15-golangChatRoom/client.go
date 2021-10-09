69667ce66ecbcf21594e806c2abffd425b0b8515
mrchi <chiqingjun@gmail.com>
2021-10-09 09:15:04 +0800

    Learned p48

--- Golang-Tutorial-Video-By-AceId/15-golangChatRoom/client.go
+++ Golang-Tutorial-Video-By-AceId/15-golangChatRoom/client.go
@@ -11,6 +11,45 @@ type Client struct {
 	ServerIp   string
 	ServerPort int
 	conn       net.Conn
+	flag       int
+}
+
+func (this *Client) Menu() bool {
+	var flag int
+	fmt.Println("1. 广播模式")
+	fmt.Println("2. 私聊模式")
+	fmt.Println("3. 修改用户名")
+	fmt.Println("0. 退出")
+
+	fmt.Scanln(&flag)
+	if flag >= 0 && flag <= 3 {
+		this.flag = flag
+		return true
+	} else {
+		fmt.Println("输入不合法")
+		return false
+	}
+}
+
+func (this *Client) Run() {
+	// 如果 flag 为 0 就退出
+	for this.flag != 0 {
+		// 如果输入不合法就一直循环
+		for this.Menu() != true {
+		}
+		// 根据不同模式区分业务
+		switch this.flag {
+		case 1:
+			fmt.Println("进入广播模式")
+			break
+		case 2:
+			fmt.Println("进入私聊模式")
+			break
+		case 3:
+			fmt.Println("进入修改用户名模式")
+			break
+		}
+	}
 }
 
 // 创建客户端
@@ -27,6 +66,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
 		conn:       conn,
+		flag:       999,
 	}
 	return &client
 }
@@ -34,7 +74,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 var serverIp string
 var serverPort int
 
-// 一把在 init 中定义命令行参数
+// 一般在 init 中定义命令行参数
 func init() {
 	flag.StringVar(&serverIp, "ip", "127.0.0.1", "服务器 IP")
 	flag.IntVar(&serverPort, "port", 8000, "服务器端口号")
@@ -42,7 +82,7 @@ func init() {
 
 func main() {
 	flag.Parse()
-	NewClient(serverIp, serverPort)
+	client := NewClient(serverIp, serverPort)
 
-	select {}
+	client.Run()
 }
