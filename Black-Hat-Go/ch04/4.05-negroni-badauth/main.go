2146d484f6129236a9fbad90bce05fe68449375b
mrchi <i@mrchi.cc>
2023-09-10 12:21:09 +0800

    c

---
+++ Black-Hat-Go/ch04/4.05-negroni-badauth/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+const LISTENING_ADDR string = "localhost:8000"
+
+type badAuth struct {
+	Username string
+	Password string
+}
+
+func (ba *badAuth) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	username := r.URL.Query().Get("username")
+	password := r.URL.Query().Get("password")
+	if username != ba.Username || password != ba.Password {
+		http.Error(w, "401 Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	ctx := context.WithValue(r.Context(), "username", username)
+	r = r.WithContext(ctx)
+
+	next(w, r)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	username := r.Context().Value("username").(string)
+	fmt.Fprintf(w, "Hello, %s", username)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", hello).Methods("GET")
+
+	n := negroni.Classic()
+	n.Use(&badAuth{Username: "foo", Password: "bar"})
+	n.UseHandler(r)
+
+	log.Printf("Listening on %s\n", LISTENING_ADDR)
+	http.ListenAndServe(LISTENING_ADDR, n)
+}
