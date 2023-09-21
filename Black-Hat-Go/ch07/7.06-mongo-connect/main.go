eb21faa8b9c518a88b3d1ed47434fa3f5fc97ec0
mrchi <i@mrchi.cc>
2023-09-21 23:51:09 +0800

    c

--- Black-Hat-Go/ch07/7.06-mongo-connect/main.go
+++ Black-Hat-Go/ch07/7.06-mongo-connect/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+const MongoURI = "mongodb://localhost:27017"
 
 type Top250Movie struct {
 	ID          string   `bson:"_id"`
 	Rank        int      `bson:"rank"`
 	Name        string   `bson:"name"`
-	RatingValue float32  `bson:"rating_value"`
+	RatingValue float64  `bson:"rating_value"`
 	RatingCount int      `bson:"rating_count"`
 	Quote       string   `bson:"quote"`
 	Year        string   `bson:"year"`
@@ -22,11 +26,25 @@ type Top250Movie struct {
 }
 
 func main() {
+	var movie Top250Movie
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoURI))
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	collection := client.Database("test").Collection("movie_top250")
+	if err = collection.FindOne(ctx, bson.M{"rank": 1}).Decode(&movie); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%#v\n", movie)
 }
